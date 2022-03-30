83214f90292d860bd67e0a6d62b4c53d675c5440
JoseDiazG <josethp@hotmail.com>
2022-03-29 21:18:00 -0500

    Finish Mini Calendar

---
+++ 29-MiniCalendar/app.js
@@ -0,0 +1,19 @@
+const monthNameEl = document.getElementById("month-name");
+const dayNameEl = document.getElementById("day-name");
+const dayNumEl = document.getElementById("day-number");
+const yearEl = document.getElementById("year");
+
+const date = new Date();
+const month = date.getMonth();
+
+monthNameEl.innerHTML = date.toLocaleString("es", {
+    month:"long"
+})
+
+dayNameEl.innerHTML = date.toLocaleString("es", {
+    weekday:"long"
+})
+
+dayNumEl.innerText = date.getDate()
+
+yearEl.innerText = date.getFullYear()
